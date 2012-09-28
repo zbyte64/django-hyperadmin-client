9bb80c8cc0961a17559d03dcb4f6d2b69008bb45
Jason Kraus <jasonk@cukerinteractive.com>
2012-09-28 15:24:08 -0700

    template fixes

--- hyperadminclient/mediatypes.py
+++ hyperadminclient/mediatypes.py
@@ -4,9 +4,4 @@ class AdminHtml5MediaType(Html5MediaType):
     default_namespace = 'adminhtmlclient'
     template_name = 'hyperadminclient/resource.html'
     template_dir_name = 'hyperadminclient'
-    
-    def get_template_names(self):
-        ret = super(AdminHtml5MediaType, self).get_template_names()
-        print ret
-        return ret
 
